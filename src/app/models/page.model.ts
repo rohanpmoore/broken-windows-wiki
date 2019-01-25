d3e7cb6f1b31fa22b5cadc30c691d17d8951a5e4
Rohan Moore <rohanpmoore@gmail.com>
2019-01-25 14:41:15 -0800

    add basic page constructor

--- src/app/models/page.model.ts
+++ src/app/models/page.model.ts
@@ -1,10 +1,31 @@
 import { Stats } from './pagestats.model';
 
 export class Page {
-  private stats: Stats;
+  public stats: Stats;
   private background: string;
+  private abilities: string;
 
   constructor(attributes) {
+    this.stats = new Stats(attributes.stats);
+    this.background = attributes.background;
+    this.abilities = attributes.abilities;
+  }
 
+  editBackground(editor, background) {
+    this.background = background;
+    this.stats.update(editor);
+  }
+
+  editAbilities(editor, abilities) {
+    this.abilities = abilities;
+    this.stats.update(editor);
+  }
+
+  getBackground() {
+    return this.background;
+  }
+
+  getAbilities() {
+    return this.abilities;
   }
 }
