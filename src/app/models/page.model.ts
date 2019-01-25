177f4969e7a782025a1659a0b78629d274d50c94
Rohan Moore <rohanpmoore@gmail.com>
2019-01-25 10:32:56 -0800

    more model work

---
+++ src/app/models/page.model.ts
@@ -0,0 +1,8 @@
+import { Stats } from './pagestats.model';
+import { Content } from '/pagecontent.model.ts';
+
+export class Page {
+  constructor(public title: string, public pageContent[]: Content[], public stats[]: Stats[]) {
+    
+  }
+}
---
+++ src/app/models/pagecontent.model.ts
@@ -0,0 +1,5 @@
+export class Content {
+  constructor(public header: string, public content: string) {
+    
+  }
+}
